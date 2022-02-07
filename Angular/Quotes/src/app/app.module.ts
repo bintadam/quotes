d9a4f9d0db52aeb1b5d96b498d5de7f66a697fe6
zakiya adam <zakiyaadan12@gmail.com>
2022-02-07 19:52:37 +0300

    added services folder and quotes.services.ts file

---
+++ Angular/Quotes/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuotesComponent,
+    QuoteDetailComponent,
+    QuoteFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
