ea4c879f4038ef8bfbb74ef81e5fc256b3103574
zakiya adam <zakiyaadan12@gmail.com>
2022-02-08 00:45:49 +0300

    added quote-detail folder with components file

--- Angular/Quotes/src/app/app.module.ts
+++ Angular/Quotes/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuotesComponent } from './quotes/quotes.component';
-import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
-import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    QuotesComponent,
-    QuoteDetailComponent,
-    QuoteFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
