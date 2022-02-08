c8de0a48bc9596198088dd52c43b74162bbeacb5
zakiya adam <zakiyaadan12@gmail.com>
2022-02-09 00:26:09 +0300

    added import bootstrap to styles.css

--- Angular/Quote/src/app/add-quotes/add-quotes.component.ts
+++ Angular/Quote/src/app/add-quotes/add-quotes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-add-quotes',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-quotes.component.css']
 })
 export class AddQuotesComponent implements OnInit {
+ 
+  newQuote = new Quotes(0,'','','',0,0,'');
+
+  @Output() addQuote= new EventEmitter<Quotes>();
+
+
+  submitQuote(){
+    console.log(this.newQuote)
+    this.addQuote.emit(this.newQuote);
+    
+  }
+
 
   constructor() { }
 
---
+++ Angular/Quote/src/app/quotes/quotes.ts
@@ -0,0 +1,16 @@
+
+  
+export class Quotes {
+   
+  constructor(
+      public id:number,
+      public author: string,
+      public title: string,
+      public quote: string,
+      public upvote: number,
+      public downvote: number,
+      public submitedBy: string,
+       ){}
+          
+       
+}
