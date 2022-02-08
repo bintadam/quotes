09502921303fea50549faf19327431f2e542c169
zakiya adam <zakiyaadan12@gmail.com>
2022-02-08 19:59:39 +0300

    added implements OnInt to export class AppComponent

--- Angular/Quotes/src/app/app.component.ts
+++ Angular/Quotes/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Quote } from '@angular/compiler/public_api';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   title = "Bint's Quotes";
+  loading: boolean = true
+  quote!: Quote
+  quoteList!: Quote[]
+  tweetURL!: string
 }
