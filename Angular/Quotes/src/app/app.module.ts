32702ca7a1f050ddad7c0dd71772e5dcd5c0c13c
zakiya adam <zakiyaadan12@gmail.com>
2022-02-10 23:13:38 +0300

    added form

--- Angular/Quotes/src/app/app.module.ts
+++ Angular/Quotes/src/app/app.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
-import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
 import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuotesDetailsComponent,
-    QuotesFormComponent
+    QuotesFormComponent,
+    QuotesDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    FormsModule, 
     ReactiveFormsModule
   ],
   providers: [],
