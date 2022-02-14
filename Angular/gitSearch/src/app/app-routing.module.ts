d602cb0843aad0b49f44556be0fd22ea34f2504a
zakiya adam <zakiyaadan12@gmail.com>
2022-02-14 23:57:09 +0300

    added routes to app-routing.module.ts

--- Angular/gitSearch/src/app/app-routing.module.ts
+++ Angular/gitSearch/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'navbar', component: NavbarComponent},
+  { path: 'search', component: SearchComponent},
+  { path:'**', component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
