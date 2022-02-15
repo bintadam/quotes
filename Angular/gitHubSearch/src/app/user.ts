53d2a91a7a29c3d73931a9ffb8d5b6f6c06418d3
zakiya adam <zakiyaadan12@gmail.com>
2022-02-15 18:09:39 +0300

    added arrays

--- Angular/gitHubSearch/src/app/user.ts
+++ Angular/gitHubSearch/src/app/user.ts
@@ -1,2 +1,8 @@
 export class User {
+  constructor(public name: any,
+     public avatar_url: any,
+     public bio: any, 
+     public following: any, 
+     public followers: any, 
+    public public_repos : any){}
 }
