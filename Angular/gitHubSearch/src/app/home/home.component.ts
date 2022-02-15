ff292004e5bf5e3359797701059770c4ecfd070b
zakiya adam <zakiyaadan12@gmail.com>
2022-02-15 17:43:30 +0300

    added findUser

--- Angular/gitHubSearch/src/app/home/home.component.ts
+++ Angular/gitHubSearch/src/app/home/home.component.ts
@@ -7,15 +7,13 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core'
 })
 export class HomeComponent implements OnInit {
 
-  
-
-  // value!: string;
-  // @Output() valueEmitter= new EventEmitter<any> ();
+  value!: string;
+  @Output() valueEmitter= new EventEmitter<any> ();
   
   constructor() { }
-  // user(){
-  //   this.valueEmitter.emit(this.value);
-  // }
+  user(){
+    this.valueEmitter.emit(this.value);
+  }
 
   ngOnInit(): void {
   }
--- Angular/gitHubSearch/src/app/user-service/user.service.ts
+++ Angular/gitHubSearch/src/app/user-service/user.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  user: Users[]=[];
+  _URL ='https://api.github.com/users/';
+  token = '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';
+
+  constructor(private http:HttpClient) { }
+  findUser(value: string){
+    interface ApiRespond{
+      avatar_url: string;
+      bio: string;
+      name : string;
+      followers : string;
+      following : string;
+      public_repos : String ;
+    }
+  }
 }
