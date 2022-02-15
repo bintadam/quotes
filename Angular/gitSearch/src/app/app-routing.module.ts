a2d092de43ebbd7f1815179dfc33c3572b7b4b08
zakiya adam <zakiyaadan12@gmail.com>
2022-02-15 12:13:04 +0300

    added and h2 to my repositories component

--- Angular/gitSearch/src/app/app-routing.module.ts
+++ Angular/gitSearch/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
 
 const routes: Routes = [
-  { path: 'Home', component: HomeComponent},
+  { path: 'Search', component: SearchComponent},
   { path: 'Repositories', component: RepositoriesComponent},
   { path:'**', component:NotFoundComponent},
+
+  { path: '', redirectTo:"/Home", pathMatch:"full"},
 ];
 
 @NgModule({
